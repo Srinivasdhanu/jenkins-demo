14639c4d1d89d83ce16022caae3f56f543bd2049
srinivas <srinivaskollipaka1319@gmail.com>
2022-04-09 23:57:11 +0530

    docker web application

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my first docker application");
+  res.send(index.html);
 });
 
-app.get("/me", (req, res) => {
-  res.send("Hi I am Laith");
-});
 
 app.listen(5000, () => {
   console.log("listening");
