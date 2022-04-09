f297db13005fb4bb63d41495628c49a27b52be0b
srinivas <srinivaskollipaka1319@gmail.com>
2022-04-09 22:07:09 +0530

    modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express app");
+  res.send("This is my first docker application");
 });
 
 app.get("/me", (req, res) => {
